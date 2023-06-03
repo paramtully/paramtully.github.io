09387ab6253d5edcf6e240b45802f13ffdedcbc4
paramtully <ptully49@students.cs.ubc.ca>
2023-06-02 23:09:21 -0700

    set up create-react-app w/ gh-pages

--- source.js
+++
@@ -1,7 +0,0 @@
-import { useState } from 'react';
-import { createRoot } from 'react-dom.client';
-import { ProfilePhoto } from './components/about';
-
-const rootElt = document.getElementById('root');
-const root = createRoot(rootElt);
-root.render(<ProfilePhoto />);
