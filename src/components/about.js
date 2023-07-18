47b60adccb1d0bc596a4fb6cea0c0e184b4e2be4
paramtully <ptully49@students.cs.ubc.ca>
2023-07-17 18:13:14 -0700

    added about me section, links, and styled nav bar

--- src/components/about.js
+++ src/components/about.js
@@ -8,16 +8,23 @@ export default function About() {
         <section id='about'>
             <h2 className="sectionTitle">ABOUT</h2>
             <p>
-                {/* Who am i? */}
-                Who am i 
+                {/* Who am i? / how did i get into SWE */}
+                My career as a software engineer began when I decided to take an intro CS course as an elective 
+                in my second year of university. I immediately fell in love with the endless possibilities of 
+                what I could create. My curiosity of the many fields in CS and their applications have guided me
+                through my degree and will continue to carry me far into the future.
             </p>
             <p>
                 {/* mission stuff */}
-                mission stuff
+                My current focus is on learning all aspects of full-stack web development, including the 
+                implementation of CI/CD solutions. My goal is to be able to independently produce applications 
+                from conception at scale.
             </p>
             <p>
-                 {/* outside of work stuff */}
-                 outside of work stuff
+                {/* outside of work stuff */}
+                When I'm not at my computer, You'll often find me working out, playing basketball either at my 
+                local YMCA or in UBC's intramural basketball league, exploring the food scene in Vancouver, 
+                or watching a nice and relaxing horror movie.
             </p>
         </section>
     );
