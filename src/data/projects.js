fe7537f1fa41a644b28bb43f5387c672a5e68d97
paramtully <ptully49@students.cs.ubc.ca>
2023-11-19 17:54:52 -0800

    fixed eslint warnings

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,15 +1,4 @@
-const projectTemplate = 
-{
-    title: null,
-    position: null,
-    detail: null,
-    date: null,
-    description: null,
-    technologies: null,
-    code_url: null,
-    project_url: null,
-    shortlist: true
-};
+
 
 const academicProjects = [
     {
