3aef47a458ef0ee75321b3c01f0fc4abfe7aa4ad
paramtully <ptully49@students.cs.ubc.ca>
2023-11-19 17:59:47 -0800

    fixed a type in invoke description

--- src/data/experience.js
+++ src/data/experience.js
@@ -16,7 +16,7 @@ const positions = [
         position: 'DevOps Engineer Co-op',
         detail: "A small company based in Vancouver that specializes in helping build tech start-ups",
         date: 'Sept 2023 - Dec 2023',
-        description: "As a DevOps Engineer, I specialized building and maintaining automation infrastructure for various projects. My first project involved automating the deployment of a React Native application to both the IOS and Android app stores. This involved learning and applying security best practices when storing and sharing sensitive data like private keys. I implemented the user authentication system in the same mobile application utilizing AWS resources.",
+        description: "In this position, I specialized in building and maintaining automation infrastructure for various projects. My first project involved automating the deployment of a React Native application to both the IOS and Android app stores. This involved learning and applying security best practices when storing and sharing sensitive data like private keys. I also implemented the user authentication system in the same mobile application utilizing AWS resources including AWS Cognito.",
         technologies: ['AWS', 'CircleCI', 'Pulumi', 'Fastlane'],
         code_url: null,
         project_url: null,
