47b8e265bd4a0501446658d372dc276420a952bc
paramtully <ptully49@students.cs.ubc.ca>
2023-11-19 17:10:28 -0800

    - added github actions workflow - updated description of invoke

--- src/data/experience.js
+++ src/data/experience.js
@@ -12,12 +12,12 @@ const positions = [
         shortlist: true
     },
     {
-        title: 'Invoke Media',
+        title: 'Invoke',
         position: 'DevOps Engineer Co-op',
-        detail: null,
-        date: 'Sept 2023 - Present',
-        description: null,
-        technologies: ['AWS', 'Pulumi', 'Terraform', 'Fastlane'],
+        detail: "A small company based in Vancouver that specializes in helping build tech start-ups",
+        date: 'Sept 2023 - Dec 2023',
+        description: "As a DevOps Engineer, I specialized building and maintaining automation infrastructure for various projects. My first project involved automating the deployment of a React Native application to both the IOS and Android app stores. This involved learning and applying security best practices when storing and sharing sensitive data like private keys. I implemented the user authentication system in the same mobile application utilizing AWS resources.",
+        technologies: ['AWS', 'CircleCI', 'Pulumi', 'Fastlane'],
         code_url: null,
         project_url: null,
         shortlist: true
