51d108ba2a32e72f103122707f42dd0b2a5dbc79
paramtully <ptully49@students.cs.ubc.ca>
2024-01-01 12:05:52 -0800

    removed invoke description bc already linked their website

--- src/data/experience.js
+++ src/data/experience.js
@@ -16,7 +16,7 @@ const positions = [
         title: 'Invoke',
         title_url: 'https://invokedigital.co/',
         position: 'DevOps Engineer Co-op',
-        detail: "Vancouver based company specializing in building start-ups",
+        detail: null,
         date: 'Sept 2023 - Dec 2023',
         description: "I worked on 2 major projects for Incrowd, a live-streaming mobile app. The first being an automated deployment pipeline to the IOS App Store which improved deploy times by 3x and no longer required manual intermediate steps. My second major project involved building a custom user authentication flow which will soon make it to production.",
         technologies: ['AWS', 'CircleCI', 'Terraform', 'React Native', 'GraphQL' ,'Fastlane'],
