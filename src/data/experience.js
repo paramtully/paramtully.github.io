8bb332358116d655e1a9a1b189f0ce4727423922
paramtully <ptully49@students.cs.ubc.ca>
2023-12-31 21:35:19 -0800

    - changed incorrect capitalization in invoke description

--- src/data/experience.js
+++ src/data/experience.js
@@ -18,7 +18,7 @@ const positions = [
         position: 'DevOps Engineer Co-op',
         detail: "Vancouver based company specializing in building start-ups",
         date: 'Sept 2023 - Dec 2023',
-        description: "I worked on 2 major projects for Incrowd, a live-streaming mobile app. The first being an automated deployment pipeline to the IOS App Store which improved deploy times by 3x and no longer required manual intermediate steps. My second major project involved building a custom user Authentication flow which will soon make it to production.",
+        description: "I worked on 2 major projects for Incrowd, a live-streaming mobile app. The first being an automated deployment pipeline to the IOS App Store which improved deploy times by 3x and no longer required manual intermediate steps. My second major project involved building a custom user authentication flow which will soon make it to production.",
         technologies: ['AWS', 'CircleCI', 'Terraform', 'React Native', 'GraphQL' ,'Fastlane'],
         code_url: null,
         project_url: "https://www.incrowd.live/",
