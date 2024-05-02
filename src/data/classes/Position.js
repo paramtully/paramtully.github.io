90bdb15bd0bbaf000d284394651fd981252777d6
paramtully <ptully49@students.cs.ubc.ca>
2024-05-01 23:02:29 -0700

    implemented position class to extend tasks; updated invoke description in positions

--- src/data/classes/Position.js
+++ src/data/classes/Position.js
@@ -1,5 +1,16 @@
 import Task from "./Task";
 
 export default class Position extends Task {
-    // stub
+    position: String;
+    required_position_keys = ['position'];
+
+    constructor(data: Any) {
+        super(data);
+        const position_keys = Object.keys(data);
+        if (!this.required_position_keys.every(key => position_keys.includes(key))) {
+            throw new Error('Missing required company keys.');
+        }
+
+        this.position = data.position;
+    }
 }
--- src/data/positions.js
+++ src/data/positions.js
@@ -18,12 +18,39 @@ const positions = [
         position: 'DevOps Engineer Co-op',
         detail: null,
         date: 'Sept 2023 - Dec 2023',
-        description: "I worked on 2 major projects for Incrowd, a live-streaming mobile app. The first being an automated deployment pipeline to the IOS App Store which improved deploy times by 3x and no longer required manual intermediate steps. My second major project involved building a custom user authentication flow which will soon make it to production.",
+        description: "I worked on 2 major projects for Incrowd, a live-streaming mobile app. The first being an automated deployment pipeline to the IOS App Store which improved deploy speeds by 3x and no longer required manual intermediate steps. My second major project involved building a custom user authentication flow which will soon make it to production.",
         technologies: ['AWS', 'CircleCI', 'Terraform', 'React Native', 'GraphQL' ,'Fastlane'],
         code_url: null,
         project_url: "https://www.incrowd.live/",
         shortlist: true
     },
 ];
+
+const positions_updated = [
+    {
+        title: null,
+        position: null,
+        summary: null,
+        description: null,
+        technologies: null,
+        start_date: null,
+        end_date: null,
+        github_url: null,
+        live_url: null,
+        org_url: null
+    },
+    {
+        title: 'Invoke',
+        position: 'DevOps Engineer Co-op',
+        summary: 'Built automated deployment pipeline for a React Native app, supporting multiple build environments and implemented login flow for the same app.',
+        description: "I worked on 2 major projects for Incrowd, a live-streaming mobile app. The first being an automated deployment pipeline to the IOS App Store which improved deploy speeds by 3x and no longer requires manual intermediate steps. The pipeline supports running and reporting automated tests on feature branches, and additionally deployment of a beta or production version of the application depending on the environment. My second major project involved building a custom user login flow which will soon make it to production. I utilized AWS Amplify for it's API and implemented the client side logic to support it.",
+        technologies: ['CircleCI', 'AWS Amplify', 'AWS S3', 'Fastlane', 'React Native', 'Terraform'],
+        start_date: new Date(2023, 9),
+        end_date: new Date(2023, 12),
+        github_url: null,
+        live_url: "https://www.incrowd.live/",
+        org_url: 'https://invokedigital.co/',
+    },
+]
 
 export { positions };
