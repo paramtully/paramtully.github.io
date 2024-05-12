3dbcbd1a390507aae5796032fb753c78bc1e1448
paramtully <ptully49@students.cs.ubc.ca>
2024-05-12 09:52:16 -0700

    removed profile photo from about

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,8 +1,4 @@
 
-function ProfilePhoto() {
-    return <img src="https://media.licdn.com/dms/image/C5603AQFFgv2nb1LeKA/profile-displayphoto-shrink_100_100/0/1659311137899?e=1691020800&v=beta&t=HZU3w3WP97rACVwVbXeCGM684y9UZv7WG6dcQ7uUUDI" alt="Param Tully"/>;
-}
-
 export default function About() {
     return (
         <section id='about'>
@@ -27,5 +23,3 @@ export default function About() {
         </section>
     );
 }
-
-export { ProfilePhoto };
